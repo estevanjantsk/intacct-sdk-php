1d4050591c191ee5ee84902694de11afff0bfdd4
priit.matiisen <priit.matiisen@scoro.com>
2023-11-21 13:33:37 +0200

    - add (ar)adjustments update support - fix (ap)bill/(ar)invoice update

---
+++ src/Intacct/Functions/AccountsReceivable/ArAdjustmentUpdate.php
@@ -0,0 +1,54 @@
+<?php
+namespace Intacct\Functions\AccountsReceivable;
+
+use Intacct\Xml\XMLWriter;
+use InvalidArgumentException;
+
+/**
+ * Create a new accounts receivable adjustment record
+ */
+class ArAdjustmentUpdate extends AbstractArAdjustment {
+
+	public function writeXml(XMLWriter &$xml)
+	{
+		$xml->startElement('function');
+		$xml->writeAttribute('controlid', $this->getControlId());
+
+		$xml->startElement('update');
+		$xml->startElement('aradjustment');
+
+		$xml->writeElement('RECORDNO', $this->getRecordNo());
+
+		$xml->writeElement('customerid', $this->getCustomerId(), true);
+
+		$xml->writeElement('WHENCREATED', $this->getTransactionDate()->format('Y-m-d'));
+
+		$xml->writeElement('action', $this->getAction());
+		$xml->writeElement('batchkey', $this->getSummaryRecordNo());
+		$xml->writeElement('RECORDID', $this->getInvoiceNumber());
+		$xml->writeElement('DOCNUMBER', $this->getAdjustmentNumber());
+		$xml->writeElement('description', $this->getDescription());
+		$xml->writeElement('externalid', $this->getExternalId());
+
+
+		$this->writeXmlMultiCurrencySection($xml);
+
+		$xml->writeElement('nogl', $this->isDoNotPostToGL());
+
+		$xml->startElement('arajustmentitems');
+		if (count($this->getLines()) > 0) {
+			foreach ($this->getLines() as $line) {
+				$line->writeXml($xml);
+			}
+		} else {
+			throw new InvalidArgumentException('AR Invoice must have at least 1 line');
+		}
+		$xml->endElement(); //arajustmentitems
+
+		$xml->endElement(); //arajustmentupdate
+
+		$xml->endElement(); //update
+
+		$xml->endElement(); //function
+	}
+}
