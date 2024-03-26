e09c81d413ff3077be175c1788819ad3f0cd5c56
priit.matiisen <priit.matiisen@scoro.com>
2024-03-26 18:12:47 +0200

    - fix "memo" field for update requests is actually "entrydescription"

--- src/Intacct/Functions/AccountsPayable/ApBillLineUpdate.php
+++ src/Intacct/Functions/AccountsPayable/ApBillLineUpdate.php
@@ -1,36 +1,14 @@
 <?php
 
-/**
- * Copyright 2021 Sage Intacct, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"). You may not
- * use this file except in compliance with the License. You may obtain a copy
- * of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * or in the "LICENSE" file accompanying this file. This file is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
-
-namespace Intacct\Functions\AccountsReceivable;
+namespace Intacct\Functions\AccountsPayable;
 
 use Intacct\Xml\XMLWriter;
 
-/**
- * Create a new accounts receivable adjustment line record
- */
-class ArAdjustmentLineUpdate extends AbstractArAdjustmentLine {
+class ApBillLineUpdate extends AbstractBillLine {
 
-	/**
-	 * Write the lineitem block XML
-	 *
-	 * @param XMLWriter $xml
-	 */
-	public function writeXml(XMLWriter &$xml) {
-		$xml->startElement('aradjustmentitem');
+	public function writeXml(XMLWriter &$xml)
+	{
+		$xml->startElement('apbillitem');
 
 		if (!empty($this->getAccountLabel())) {
 			$xml->writeElement('accountlabel', $this->getAccountLabel(), true);
@@ -44,6 +22,9 @@ class ArAdjustmentLineUpdate extends AbstractArAdjustmentLine {
 		$xml->writeElement('entrydescription', $this->getMemo());
 		$xml->writeElement('locationid', $this->getLocationId());
 		$xml->writeElement('departmentid', $this->getDepartmentId());
+		$xml->writeElement('item1099', $this->isForm1099());
+		$xml->writeElement('form1099type', $this->getForm1099type());
+		$xml->writeElement('form1099box', $this->getForm1099box());
 		$xml->writeElement('key', $this->getKey());
 		$xml->writeElement('totalpaid', $this->getTotalPaid());
 		$xml->writeElement('totaldue', $this->getTotalDue());
@@ -58,9 +39,10 @@ class ArAdjustmentLineUpdate extends AbstractArAdjustmentLine {
 		$xml->writeElement('classid', $this->getClassId());
 		$xml->writeElement('contractid', $this->getContractId());
 		$xml->writeElement('warehouseid', $this->getWarehouseId());
+		$xml->writeElement('billable', $this->isBillable());
 
 		// if there are tax entries, lets add them to our xml
-		if (!empty($this->getTaxEntry())) {
+		if(!empty($this->getTaxEntry())) {
 			$xml->startElement('taxentries');
 			foreach ($this->getTaxEntry() as $taxentry) {
 				$taxentry->writeXml($xml);
--- src/Intacct/Functions/AccountsReceivable/InvoiceLineUpdate.php
+++ src/Intacct/Functions/AccountsReceivable/InvoiceLineUpdate.php
@@ -1,36 +1,14 @@
 <?php
 
-/**
- * Copyright 2021 Sage Intacct, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"). You may not
- * use this file except in compliance with the License. You may obtain a copy
- * of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * or in the "LICENSE" file accompanying this file. This file is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
 
 namespace Intacct\Functions\AccountsReceivable;
 
 use Intacct\Xml\XMLWriter;
 
-/**
- * Create a new accounts receivable adjustment line record
- */
-class ArAdjustmentLineUpdate extends AbstractArAdjustmentLine {
+class InvoiceLineUpdate extends AbstractInvoiceLine {
 
-	/**
-	 * Write the lineitem block XML
-	 *
-	 * @param XMLWriter $xml
-	 */
 	public function writeXml(XMLWriter &$xml) {
-		$xml->startElement('aradjustmentitem');
+		$xml->startElement('arinvoiceitem');
 
 		if (!empty($this->getAccountLabel())) {
 			$xml->writeElement('accountlabel', $this->getAccountLabel(), true);
@@ -49,6 +27,19 @@ class ArAdjustmentLineUpdate extends AbstractArAdjustmentLine {
 		$xml->writeElement('totaldue', $this->getTotalDue());
 
 		$this->writeXmlExplicitCustomFields($xml);
+
+		$xml->writeElement('revrectemplate', $this->getRevRecTemplateId());
+		$xml->writeElement('defrevaccount', $this->getDeferredRevGlAccountNo());
+		if ($this->getRevRecStartDate()) {
+			$xml->startElement('revrecstartdate');
+			$xml->writeDateSplitElements($this->getRevRecStartDate(), true);
+			$xml->endElement();
+		}
+		if ($this->getRevRecEndDate()) {
+			$xml->startElement('revrecenddate');
+			$xml->writeDateSplitElements($this->getRevRecEndDate(), true);
+			$xml->endElement();
+		}
 
 		$xml->writeElement('projectid', $this->getProjectId());
 		$xml->writeElement('customerid', $this->getCustomerId());
